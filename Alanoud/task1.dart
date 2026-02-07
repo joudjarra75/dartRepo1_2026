78376aab4b3f7174a65e347f2770349c2077ed0e
Joud Al-jarrah <72281607+joudjarra75@users.noreply.github.com>
2026-02-07 15:22:16 +0300

    Update task1.dart

--- Alanoud/task1.dart
+++ Alanoud/task1.dart
@@ -1,3 +1,13 @@
 main(){
-  print ("hello");
+
+// int var called age
+int age; //dec
+age = 20; //init
+
+String name = "saif";
+String major = "IT";
+print("Hi I'm $name , I'm $age years old");
+
+
+
 }
