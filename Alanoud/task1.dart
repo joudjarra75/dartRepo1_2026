27fd24b14d352f80a337ef16ea0e1d289ca8405f
anood-taleb <AlAnood.taleb88@gmail.com>
2026-02-08 15:53:35 +0300

    Update task1.dart

--- Alanoud/task1.dart
+++ Alanoud/task1.dart
@@ -1,13 +1,21 @@
-main(){
-
-// int var called age
-int age; //dec
-age = 20; //init
-
-String name = "saif";
-String major = "IT";
-print("Hi I'm $name , I'm $age years old");
-
-
-
-}
+// santax are ruls the we have to folow ex(we always start with main)
+// remembaring () and {} and always having a ; in the end to avoind errors
+// block dart we have a start and an end and any thing in is what we say inside the block 
+// print you always have to type it befor the thing you want to print
+//ex
+main(){//start
+print("anood");
+}//end
+// Data typs (Numbers,string.boolean,list,maps)
+int (num) 13
+double  (num) 1.30
+bool (true/false)
+string ('hello' "world")
+// variabls holds data and value
+int a=7; // a is the variable and 7 is the value
+// string concatenation is when we add + in the midle of 2 names for example
+'play'+'time' = playtime
+// string interpolation is when we add $ 
+name=anood
+from=Jordan
+hello ${anood}, welcom to ${from}.
