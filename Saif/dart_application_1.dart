be3caf619eaa6d82a53ab64bb181774de02639b9
SaifEddine8 <saifbnyslamh12@gmail.com>
2026-02-09 22:23:49 +0300

    Add files via upload

---
+++ Saif/dart_application_1.dart
@@ -0,0 +1,32 @@
+void main()
+{/*
+variable is locations in memory
+it is use to store things inside it
+*/
+// to store number use: int, double
+// int to integer
+int number;
+//double to real number
+double floating;
+//store string use: String
+String  word;
+//store true or false use: bool
+bool option;
+
+/* print method is used to print any thing passed to it
+   if passed many strings we need concat sign(+)
+ */
+print("saif"+" "+"Eddin");
+//or we can use interpoliate sign($)
+String name="Eddin";
+print("saif $name");
+
+//if we wnat to formate the string passed to print in multi-line use:
+//\n
+//''' or """
+print("saif Eddin \nBani Salameh");
+print("""saif Eddin 
+Bani Salameh""");
+print('''saif Eddin 
+Bani Salameh''');
+}
