3dfe50d191935d3bbbffeff359f77950e3f748c9
SaifEddine8 <saifbnyslamh12@gmail.com>
2026-02-14 11:06:49 +0300

    Add files via upload

---
+++ Saif/task2_q1.dart
@@ -0,0 +1,7 @@
+void main()
+{
+print("Hello, in this code we will using \"Escape charachters\"\nthis is backslash \\\nthis is double quotes \"\nthis is single quotes \',\nwe can print new line using \" \\n \"\nand print spaces using \" \\t \"");
+
+
+
+}
