c246bab419979706f243a30f082410b9fc39d377
anood-taleb <AlAnood.taleb88@gmail.com>
2026-02-18 13:34:53 +0300

    Update and rename task2_q1.dart to task2

--- Alanoud/task2_q1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  print("Hello, in this code we will be using \"Escape characters\"\n"
-        "This is backslash \\\n"
-        "This is double quotes \"\n"
-        "This is single quotes \'\n"
-        "we can print new line using \" \\n \"\n"
-        "and print spaces using \" \\t \"");
-}
